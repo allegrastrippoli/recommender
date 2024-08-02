0dcede0590321c0ac240621d61b29ba62bc22bda
Allegra Strippoli <allegra@strippoli.eu>
2024-08-02 19:47:22 +0200

    fix: create table

--- csv_sql_converter.py
+++ csv_sql_converter.py
@@ -1,27 +1,50 @@
 import pandas as pd
-from sqlalchemy import create_engine, Table, Column, Integer, String, MetaData
-from sqlalchemy.orm import sessionmaker, declarative_base, registry
-from sqlalchemy import insert
+from sqlalchemy import create_engine, Column, Integer, String
+from sqlalchemy.orm import sessionmaker, declarative_base
+from sqlalchemy import select
 
 engine = create_engine('sqlite:///recsys.sqlite')
+Session = sessionmaker(bind=engine)
+session = Session()
 
 # From sqlalchemy Docs:
 # Base: A class that links database tables with python classes
 # MetaData: A collection of Table objects and their associated schema constructs
 # The MetaData is accessed via the Base class
 Base = declarative_base()
-    
-movie_table = Table('movie_table', Base.metadata,
-                        Column('id', Integer, primary_key=True), 
-                        Column('movieId', String),
-                        Column('title', String), 
-                        Column('genres', String))
 
-rating_table = Table('rating_table', Base.metadata,
-                        Column('id', Integer, primary_key=True), 
-                        Column('userId', String),
-                        Column('movieId', String), 
-                        Column('rating', String))
+class Movie(Base):
+    __tablename__ = 'movie_table'
+    
+    id = Column(Integer, primary_key=True)
+    movieId = Column(String)
+    title = Column(String)
+    genres = Column(String)
+    
+    def __init__(self, movieId, title, genres):
+        self.movieId = movieId
+        self.title = title
+        self.genres = genres
+    
+    def __repr__(self):
+        return f'{self.movieId, self.title, self.genres}'
+    
+class Rating(Base): 
+    __tablename__ = 'rating_table'
+    
+    id = Column(Integer, primary_key=True)
+    userId = Column(String)
+    movieId = Column(String)
+    rating = Column(String)
+    
+    def __init__(self, userId, movieId, rating):
+        self.userId = userId
+        self.movieId = movieId
+        self.rating = rating
+    
+    def __repr__(self):
+        return f'{self.rating, self.movieId, self.rating}'
+
 
 def create_movie_table(filepath):
     csv_data = pd.read_csv(filepath)
@@ -30,9 +53,12 @@ def create_movie_table(filepath):
     Base.metadata.create_all(engine)
     
     for row in csv_data:
-        stmt = insert(movie_table).values(movieId=row[0], title=row[1], genres=row[2])
-        print(stmt)
+        movie = Movie(movieId=row[0], title=row[1], genres=row[2])
+        session.add(movie)
+    
+    session.commit()
 
+    print(session.query(Movie).count())
 
 def create_rating_table(filepath):
     csv_data = pd.read_csv(filepath)
@@ -41,16 +67,32 @@ def create_rating_table(filepath):
     Base.metadata.create_all(engine)
     
     for row in csv_data:
-        stmt = insert(rating_table).values(userId=row[0], movieId=row[1], rating=row[2])
-        print(stmt)
+        rating = Rating(userId=row[0], movieId=row[1], rating=row[2])
+        session.add(rating)
+    
+    session.commit()
 
+    print(session.query(Rating).count())
 
 def insert_rating(userId, movieId, rating):
-    stmt = insert(rating_table).values(userId=userId, movieId=movieId, rating=rating)
-    print(stmt)
-    compiled = stmt.compile()
-    print(compiled.params)
+    rating = Rating(userId=userId, movieId=movieId, rating=rating)
+    session.add(rating)
+    session.commit()
 
-create_movie_table("ml-latest-small/movies.csv")
-create_rating_table("ml-latest-small/ratings.csv")
-insert_rating("611","1","4.0")
+def select_movie():
+    stmt = select(Movie).where(Movie.movieId == "193585")
+    for row in session.execute(stmt):
+        print(row)
+
+# def select_rating():
+#     stmt = select(Rating).where(Rating.userId == "1")
+#     for row in session.execute(stmt):
+#         print(row)
+
+
+# create_movie_table("ml-latest-small/movies.csv")
+# create_rating_table("ml-latest-small/ratings.csv")
+# insert_rating("611", "1", "1")
+
+
+
